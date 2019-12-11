e1136e5b76a1b3455efa82d21875ad224f6e19b0
Marko <maricic92marko@gmail.com>
2019-12-11 13:57:36 +0100

    c

---
+++ WMTest/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using WMTest.Models;
+using System.Data.SqlClient;
+
+namespace WMTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        // GET: Home
+        public ActionResult Index(List<Products> data)
+        {
+
+            try
+            {
+                var prod = new Products();
+  
+                ViewBag.ViewModelList = prod.LoadFromDB(null);
+                return View();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }            
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            var prod = new Products();
+            BinaryReader b = new BinaryReader(file.InputStream);
+            byte[] binData = b.ReadBytes(file.ContentLength);
+            string result = System.Text.Encoding.UTF8.GetString(binData);
+            ViewBag.MyProdList = prod.Product_JSON(result);
+            TempData["ProdJSONData"] = ViewBag.MyProdList;
+            ViewBag.ViewModelList = prod.LoadFromDB(null);
+            return View();
+        }
+
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+
+        public ActionResult Create(FormCollection collection)
+        {
+            var prod = new Products();
+            prod.CreateProd(collection);
+            return RedirectToAction("Index");
+        }
+
+       
+        public ActionResult SaveJSONToDB()
+        {
+            try
+            {
+                var li = (IEnumerable<Products>)TempData["ProdJSONData"];
+                var prod = new Products();
+                prod.SaveJSONToDB(li.ToList());
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    var prod = new Products();
+                    ViewBag.EditProd = prod.LoadFromDB(id);
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(FormCollection collection)
+        {
+            try
+            {
+               
+                var prod = new Products();
+                prod.UpdateProd(collection);
+                return RedirectToAction("Index");
+
+ 
+
+                // TODO: Add update logic here
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // POST: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var prod = new Products();
+                prod.DeleteProd(id);
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                throw e;
+                //return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ WMTest/Models/Products.cs
@@ -0,0 +1,247 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Mvc;
+
+namespace WMTest.Models
+{
+    public class Products 
+    {
+        string conString = @"Data Source=MARKO-PC\SQLEXPRESS;Initial Catalog=WMDB;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        //id, naziv, opis, kategorija, proizvođač, dobavljač, cena
+        public int id { get; set; }
+        public string naziv { get; set; }
+        public string opis { get; set; }
+        public string kategorija { get; set; }
+        public string proizvodjac { get; set; }
+        public string dobavljac { get; set; }
+        public decimal cena { get; set; }
+
+        public List<Products> Product_JSON(string result)
+        {
+            var ProdList = new List<Products>();
+            if (result.Length > 0)
+            {
+                JObject jObject = JObject.Parse(result);
+                JToken jUser = jObject["product"];
+                for (int i = 0; i < jUser.Count(); i++)
+                {
+                    var prod = new Products();
+                    prod.id = (int)jUser[i]["id"];
+                    prod.opis = (string)jUser[i]["opis"];
+                    prod.naziv = (string)jUser[i]["naziv"];
+                    prod.kategorija = (string)jUser[i]["kategorija"];
+                    prod.proizvodjac = (string)jUser[i]["proizvodjac"];
+                    prod.dobavljac = (string)jUser[i]["dobavljac"];
+                    prod.cena = (decimal)jUser[i]["cena"];
+                    ProdList.Add(prod);
+                }
+            }
+            return ProdList;
+        }
+
+        public void SaveJSONToDB(List<Products> List)
+        {
+            try
+            {
+                //product.ID, product.Naziv, product.Opis, product.Kategorija,
+                //product.Proizvodjac, product.Dobavljac, product.Cena);
+
+                foreach (var item in List)
+                {
+                    string commandText = "exec Insert_proc @Naziv,@Opis,@Kategorija,@Proizvodjac,@Dobavljac,@Cena";
+                    SqlDataReader rdr = null;
+                    SqlConnection con = new SqlConnection(conString);
+                    SqlCommand cmd = new SqlCommand(commandText, con);
+                    cmd.Parameters.Add("@ID", SqlDbType.Int);
+                    cmd.Parameters.Add("@Naziv", SqlDbType.VarChar);
+                    cmd.Parameters.Add("@Opis", SqlDbType.VarChar);
+                    cmd.Parameters.Add("@Kategorija", SqlDbType.VarChar);
+                    cmd.Parameters.Add("@Proizvodjac", SqlDbType.VarChar);
+                    cmd.Parameters.Add("@Dobavljac", SqlDbType.VarChar);
+                    cmd.Parameters.Add("@Cena", SqlDbType.Decimal);
+
+                    cmd.Parameters["@ID"].Value = item.id;
+                    cmd.Parameters["@Naziv"].Value = item.naziv;
+                    cmd.Parameters["@Opis"].Value = item.opis;
+                    cmd.Parameters["@Kategorija"].Value = item.kategorija;
+                    cmd.Parameters["@Proizvodjac"].Value = item.proizvodjac;
+                    cmd.Parameters["@Dobavljac"].Value = item.dobavljac;
+                    cmd.Parameters["@Cena"].Value = item.cena;
+
+
+                    con.Open();
+                    rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var msg = (string)rdr["msg"];
+                    }
+                }
+               
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        public List<Products> LoadFromDB(int? id)
+        {
+            SqlDataReader rdr = null;
+            SqlConnection con = new SqlConnection(conString);
+
+            string commandText = "Select * from product" ;
+            SqlCommand cmd = new SqlCommand(commandText, con);
+            if (id != null)
+            {
+                commandText += "WHERE ID = @ID";
+                cmd.Parameters.Add("@ID", SqlDbType.Int);
+                cmd.Parameters["@ID"].Value = id;
+            }
+      
+
+            con.Open();
+
+            var ViewModelList = new List<Products>();
+
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+
+
+                var prod = new Products();
+                //var iii = ii;
+                
+                prod.id = int.Parse(rdr["ID"].ToString());
+                prod.kategorija = (string)rdr["Kategorija"];
+                prod.dobavljac = (string)rdr["Dobavljac"];
+                prod.opis = (string)rdr["Opis"];
+                prod.naziv = (string)rdr["Naziv"];
+                prod.proizvodjac = (string)rdr["Proizvodjac"];
+                prod.cena = (decimal)rdr["Cena"];
+                ViewModelList.Add(prod);
+            }
+
+            con.Close();
+            return ViewModelList;
+        }
+        public void InsertProd()
+        {
+
+        }
+        public void UpdateProd(FormCollection collection)
+        {
+            try
+            {
+                //product.ID, product.Naziv, product.Opis, product.Kategorija,
+                //product.Proizvodjac, product.Dobavljac, product.Cena);
+
+                string commandText = "exec Update_proc @ID,@Naziv,@Opis,@Kategorija,@Proizvodjac,@Dobavljac,@Cena";
+                SqlDataReader rdr = null;
+                SqlConnection con = new SqlConnection(conString);
+                SqlCommand cmd = new SqlCommand(commandText, con);
+                cmd.Parameters.Add("@ID", SqlDbType.Int);
+                cmd.Parameters.Add("@Naziv", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Opis", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Kategorija", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Proizvodjac", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Dobavljac", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Cena", SqlDbType.Decimal);
+
+                cmd.Parameters["@ID"].Value = collection["id"];
+                cmd.Parameters["@Naziv"].Value = collection["naziv"];
+                cmd.Parameters["@Opis"].Value = collection["opis"];
+                cmd.Parameters["@Kategorija"].Value = collection["kategorija"];
+                cmd.Parameters["@Proizvodjac"].Value = collection["proizvodjac"];
+                cmd.Parameters["@Dobavljac"].Value = collection["dobavljac"];
+                cmd.Parameters["@Cena"].Value = collection["cena"];
+
+
+                con.Open();
+                    rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var msg = (string)rdr["msg"];
+                    }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        public void CreateProd(FormCollection collection)
+        {
+
+            try
+            {
+                //product.ID, product.Naziv, product.Opis, product.Kategorija,
+                //product.Proizvodjac, product.Dobavljac, product.Cena);
+
+                string commandText = "exec insert_proc @Naziv,@Opis,@Kategorija,@Proizvodjac,@Dobavljac,@Cena";
+                SqlDataReader rdr = null;
+                SqlConnection con = new SqlConnection(conString);
+                SqlCommand cmd = new SqlCommand(commandText, con);
+                //cmd.Parameters.Add("@ID", SqlDbType.Int);
+                cmd.Parameters.Add("@Naziv", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Opis", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Kategorija", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Proizvodjac", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Dobavljac", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Cena", SqlDbType.Decimal);
+
+                //cmd.Parameters["@ID"].Value = collection["id"];
+                cmd.Parameters["@Naziv"].Value = collection["naziv"];
+                cmd.Parameters["@Opis"].Value = collection["opis"];
+                cmd.Parameters["@Kategorija"].Value = collection["kategorija"];
+                cmd.Parameters["@Proizvodjac"].Value = collection["proizvodjac"];
+                cmd.Parameters["@Dobavljac"].Value = collection["dobavljac"];
+                cmd.Parameters["@Cena"].Value = collection["cena"];
+
+
+                con.Open();
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var msg = (string)rdr["msg"];
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+
+           
+        }
+        public void DeleteProd(int id)
+        {
+            string commandText = "exec Delete_proc @ID";
+            SqlDataReader rdr = null;
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(commandText, con);
+            cmd.Parameters.Add("@ID", SqlDbType.Int);
+            cmd.Parameters["@ID"].Value = id;
+            con.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var msg = (string)rdr["msg"];
+            }
+
+        }
+    }
+}
